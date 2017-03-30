74bbf2cb4abbca102c0f4d3d270eb2c4eb956a51
Daniel <daniget156@gmail.com>
2017-03-30 12:31:55 +0300

    Display "Null Error" Bug Fixed
    The program does not return and exception when .display is run with no user. It will print "no customer found".

--- src/PriceCalc.java
+++ src/PriceCalc.java
@@ -21,48 +21,58 @@ class PriceCalc {
             cmd = in.nextLine();
             if (cmd.equals("new")) {
                 stime = getTime();
-                System.out.print("~Name: ");
+                System.out.print("Name: ");
                 name=in.nextLine();
                 name = name.toLowerCase();
                 customer.put(name, stime);
             } else if (cmd.contains(".end")) {
                 if (customer.get(cmd.substring(0, cmd.indexOf(".")).toLowerCase()) == null) {
-                    System.err.print("~User '" + cmd.substring(0, cmd.indexOf(".")) + "' does not exist!");
+                    System.err.print("User '" + cmd.substring(0, cmd.indexOf(".")) + "' does not exist!");
                     System.out.println();
 
                 } else {
                     fee = getFee(customer, cmd);
-                    System.out.println("~Fee: " + fee + " birr");
+                    System.out.println("Fee: " + fee + " birr");
                     customer.put(cmd.substring(0, cmd.indexOf(".")).toLowerCase(), null);
                 }
             }else if (cmd.contains("display")){
                 String fullList = (Arrays.asList(customer).toString());
                 String[] fullArray = (fullList.substring(2, fullList.length()-2).split(", "));
+                if (fullArray[0].equals("")) {
+                    System.err.print ("No customers found!");
+                    System.out.println();
+                } else {
 
-                for (int i=0; i<fullArray.length; i++){
-                    String current = fullArray[i];
-                    String singlename = current.substring(0, current.indexOf("="));
-                    int time = Integer.parseInt(current.substring (current.indexOf("=")+1, current.length()));
-                    int hour = time/60;
-                    int min = time % 60;
-                    System.out.println ("Name: "+singlename);
-                    if (min<10) {
-                        System.out.println("Start time: " + hour + ":0" + min);
-                    } else {
-                        System.out.println("Start time: " + hour + ":" + min);
+                    for (int i = 0; i < fullArray.length; i++) {
+                        String current = fullArray[i];
+                        String singlename = current.substring(0, current.indexOf("="));
+                        if (!(customer.get(singlename) == null)) {
+                            int time = Integer.parseInt(current.substring(current.indexOf("=") + 1, current.length()));
+                            int hour = time / 60;
+                            int min = time % 60;
+                            System.out.println("Name: " + singlename);
+                            if (min < 10) {
+                                System.out.println("Start time: " + hour + ":0" + min);
+                            } else {
+                                System.out.println("Start time: " + hour + ":" + min);
+                            }
+                            String currCmd = singlename + ".end";
+                            System.out.println("Current fee: " + getFee(customer, currCmd));
+                            System.out.println("");
+                        } else {
+                            System.err.print ("No customers found!");
+                            System.out.println();
+                        }
+
                     }
-                    String currCmd = singlename+".end";
-                    System.out.println("Current fee: "+getFee(customer, currCmd));
-                    System.out.println("");
-
                 }
             } else if (cmd.contains(".current")) {
                 if (customer.get(cmd.substring(0, cmd.indexOf(".")).toLowerCase()) == null) {
-                    System.err.print("~User '" + cmd.substring(0, cmd.indexOf(".")) + "' does not exist!");
+                    System.err.print("User '" + cmd.substring(0, cmd.indexOf(".")) + "' does not exist!");
                     System.out.println();
                 } else {
                     double sofar = getCurrent(customer, cmd);
-                    System.out.println("~Current fee for " + cmd.substring(0, cmd.indexOf(".")) + " is " + sofar);
+                    System.out.println("Current fee for " + cmd.substring(0, cmd.indexOf(".")) + " is " + sofar);
                 }
             }
         }
